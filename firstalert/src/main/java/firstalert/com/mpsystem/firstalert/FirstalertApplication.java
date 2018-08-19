e0df1e02e30c72318b979eac18e7aee3421e7bd9
octavio <octaviobedregalflores@outlook.com>
2018-08-19 11:31:02 -0500

    Proyecto java

---
+++ firstalert/src/main/java/firstalert/com/mpsystem/firstalert/FirstalertApplication.java
@@ -0,0 +1,12 @@
+package firstalert.com.mpsystem.firstalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstalertApplication.class, args);
+	}
+}
